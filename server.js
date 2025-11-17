b378615239ffa16fd48b42908d62f30bd0626ea2
LamiaDisha <155727148+LamiaDisha@users.noreply.github.com>
2025-11-17 15:32:06 +0600

    assignment1 lab2
    assignment1 lab2

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ app.use((err, req, res, next) => {
 // In-memory task storage
 // --------------------------
 const tasks = [
-  { id: 1, title: 'Sample Task', completed: false }
+  { id: 1, title: 'Sample Task', completed: false },
+  { id: 2, title: ' Task 2', completed: false },
+  { id: 3, title: ' Task 3', completed: false },
+  { id: 4, title: ' Task 4', completed: false },
+  { id: 5, title: ' Task 5', completed: false }
+
+  
 ];
 
 app.locals.tasks = tasks;
